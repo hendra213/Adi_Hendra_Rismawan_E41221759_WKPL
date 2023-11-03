6e6dacaeb8202701ffa1a6ced07283a7ba6dd076
Adi Hendra <adihendra1303@gmail.com>
2023-11-03 20:35:45 +0700

    Commit Keempat

--- src/main/Kotlin/org/example/Calculator.kt
+++ src/main/Kotlin/org/example/Calculator.kt
@@ -2,7 +2,9 @@ package org.example
 
 class Calculator {
 
+//    Method add menerima dua parameter, yaitu first dan second,keduanya bertipe data Int.
     fun  add(first: Int, second: Int): Int {
+//    Hasil penambahan tersebut kemudian dikembalikan sebagai nilai kembalian dari method add.
         return first + second
     }
 }
--- src/test/kotlin/org/exampleTest/CalculatorTest.kt
+++ src/test/kotlin/org/exampleTest/CalculatorTest.kt
@@ -1,14 +1,23 @@
 package org.exampleTest
 
 import org.example.Calculator
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class CalculatorTest {
 
+    // Membuat instance (objek) dari kelas Calculator untuk digunakan dalam pengujian
     val calculator = Calculator()
 
     @Test
     fun testAddSuccess() {
-        val result = calculator.add(10, 10)
+        // Nilai yang diharapkan (expected) setelah menjalankan metode add(40, 2) adalah 42
+        val result = 42
+        // Memanggil metode add(40, 2) dari kelas Calculator dan membandingkannya dengan nilai result
+        // Jika hasilnya sama dengan result, maka pengujian akan berhasil
+        // setelah saya beberapa kali coba ternyata commit yang sebelumnya hanya menguji fungsi add
+        // oleh sebab itu saya menambahkan assert equals agar ketika fungsi add tidak menjumlahkan sesuai dengan result
+        // akan mengalami eror ketika yang dijumlahkan pada fungsi add benar maka tidak akan terjadi eror
+        assertEquals(result, calculator.add(40, 2))
     }
 }
