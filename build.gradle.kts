237b001dff62d40b2d385845eee09604b5cc3b0d
Adi Hendra <adihendra1303@gmail.com>
2023-11-03 17:49:35 +0700

    Commit pertama

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    kotlin("jvm") version "1.9.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(8)
+}
---
+++ src/main/Kotlin/org/example/Main.kt
@@ -0,0 +1,8 @@
+package org.example
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello world!")
+    }
+}
